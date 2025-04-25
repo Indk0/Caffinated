ab5e427e3c2fe272eae42934a3c2880d543386f7
Indk0 <inderik302@gmail.com>
2025-04-25 10:22:16 +0000

    Add all test cases

--- test_coffee_menu.py
+++ test_coffee_menu.py
@@ -5,11 +5,19 @@ class TestCoffeeMenu(unittest.TestCase):
 
     #Set code up for testing
     def setUp(self):
-        self.account = CoffeeMenu()
+        self.menu = CoffeeMenu()
     
     def tearDown(self):
-        self.account = None
+        self.menu = None
 
+    def test_get_price_exisisting_item(self):
+        self.assertEqual(self.menu)
+
+    def test_get_price_non_existing_item(self):
+        self.assertTrue(self.menu, 'Foccacia')
+
+    def test_add_item(self):
+        self.assertIn(self.menu, 'large latte')
 
 
 if __name__ == '__main__':
